b93bbbfe8c73f19f98d06ce976ee26d7aaf08cc8
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-06 21:23:57 +0900

    add:userコントローラー,showのroute追加
    マイページの実装のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show]
   devise_for :users
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
