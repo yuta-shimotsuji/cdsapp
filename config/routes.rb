720b293e2f2c17a8103ad7be9ce8ffb811c5bacf
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-06 21:33:14 +0900

    routingの順番を変更
    userコントローラーに引っかかりdvise関連が表示されなかったため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
   devise_for :users
+  resources :users, only: [:show]
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
