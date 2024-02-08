23e051f2fa638f42d2c552c97463f65e3fb593f9
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-08 21:06:48 +0900

    編集ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts, only: [:show]
+  resources :accounts, only: [:show, :edit, :update]
   devise_for :users
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
