a6bcc96db17ff1ae6533db3b049c6fc0325ef8b9
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-08 20:04:44 +0900

    usersコントローラーをaccountsコントローラーに変更
    routingがdviseと交わり複雑になってしまうため変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :accounts, only: [:show]
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
