28a4fa0790a26ff9db69958dddf6a3d8c07b74d0
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-10 15:58:05 +0900

    fix:accountコントローラー,viewのedit,update削除
    dviseの編集ページでマイページ編集を行うため

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,22 +1,8 @@
 class AccountsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update]
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
 
   def show
-  end
-
-  def edit
-    unless @user == current_user
-      redirect_to edit_account_path(@user)
-    end
-  end
-  
-  def update
-    if current_user.update(account_params)
-      redirect_to account_path(current_user)
-    else
-      redirect_to edit_account_path(current_user)
-    end
   end
   
   private
