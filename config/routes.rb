914e0b2e110dfad074b6c83a742c651003206f1a
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-17 17:34:34 +0900

    fix:アカウント登録後、編集後にユーザーページへアクセスするようにroutesの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :accounts, only: [:show]
   get '/mypage' => 'accounts#mypage'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
