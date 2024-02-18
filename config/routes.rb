2d45849d2b9d966cf0cd45dc749b4d39dc7fbda5
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-18 19:12:57 +0900

    add:ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/mypage' => 'accounts#mypage'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: "homes#top"
+  post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
