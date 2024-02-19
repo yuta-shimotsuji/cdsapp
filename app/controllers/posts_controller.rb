631f09fd2252557e98413a817497053cdc853617
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-19 23:18:51 +0900

    postコントローラーのview,routes,controllerの土台を設定
    モデルにデータを送ることができる状態を確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: "homes#top"
   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
