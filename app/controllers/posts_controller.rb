c005f4101afaa2ee963d1073d41ff61e9cd1bbff
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-26 22:25:12 +0900

    add:postコントローラーにeditアクション、viewを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,8 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @latLng = Geocoder.search(@post.address).first.coordinates
   end
 
   def update
