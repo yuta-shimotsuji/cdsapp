2ad7ad9609e30344133dc278b5484bf28a7e857f
yuta-shimotsuji <moshimo0310@gmail.com>
2024-02-29 02:13:43 +0900

    add:投稿一覧の表示日時を日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,6 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %m月 %d日 %H時 %M分'
+Time::DATE_FORMATS[:datetime]    = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:date_jp]     = '%Y年 %m月 %d日'
+Time::DATE_FORMATS[:date]        = '%Y/%m/%d'
+Time::DATE_FORMATS[:time_jp]     = '%H時 %M分 %S秒'
+Time::DATE_FORMATS[:time]        = '%H:%M:%S'
