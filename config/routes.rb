fa60076cb872fe13abb9544ed27ac56648e0d481
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-02 14:31:42 +0900

    fix:rootを投稿一覧であるposts_newに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :accounts, only: [:show]
   get '/mypage' => 'accounts#mypage'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
-  root to: "homes#top"
+  root to: "posts#new"
   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
