18c80f36e40b3d96b17ef8068806f52ee59c0abd
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-05 22:47:30 +0900

    add:postモデルにuser_idカラムを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, :body, :address, presence: true
   validates :body, length: { maximum: 140 }
+  validates :user_id, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_112147) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_05_133451) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_112147) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
