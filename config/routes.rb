b9a2a9ffb164ca6f58879070d408bff2d2d017f0
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-14 14:14:29 +0900

    add:アプリの使い方ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: "posts#new"
   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
+  get '/homes/privacy_policy', to: 'homes#privacy_policy'
+  get '/homes/terms_of_service', to: 'homes#terms_of_service'
+  get '/homes/how_to_use_the_app', to: 'homes#how_to_use_the_app'
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     collection do
