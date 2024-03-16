160437a3932938245d1d568f15566f0e08be8d6f
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-16 22:27:13 +0900

    fix:パスワード変更メールのリンクのエラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { protocol: 'https', host:'localhost:3000'}
+  config.action_mailer.default_url_options = { protocol: 'https', host:'https://cdsapp-c4875fa80e36.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
 
