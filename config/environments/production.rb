f0d5e1505ebbaba85f042df71938c3d2d7d0c9d4
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-16 22:20:52 +0900

    fix:本番環境でのメールアドレス設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { protocol: 'https', host:'localhost:3000'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address:"smtp.gmail.com",
+    domain: 'gmail.com', 
+    user_name: Rails.application.credentials.user_name, 
+    password: Rails.application.credentials.password, 
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
