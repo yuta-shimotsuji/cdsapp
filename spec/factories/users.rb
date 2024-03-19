98e5a39887d12f1f1846b737b4f4723bbc65e163
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-19 10:53:04 +0900

    add:Userモデルのテスト完成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user_#{n}@example.com"} 
-    #↑これが呼び出されるたびに、nの部分に数字が一つずつ増えて入るため、一意性が保たれる
     password {'password'}
     password_confirmation {'password'}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,16 @@ RSpec.describe User, type: :model do
 
     let(:user) { create(:user) }
     let(:posts) { create_list(:post, 3, user: user) }
+    let(:user_favorite) { create(:user) }
+    let(:post_favorite) { create(:post, user: user_favorite) }
+    let(:favorite) { create(:favorite, user_id: user_favorite.id, post_id: post_favorite.id) }
   
     it 'Postモデルとの紐付けが正しくできていること' do
       expect([user.posts]).to include posts
+    end
+
+    it 'Favoriteモデルとの紐付けが正しくできていること' do
+      expect(favorite).to be_valid
     end
   end
 end
