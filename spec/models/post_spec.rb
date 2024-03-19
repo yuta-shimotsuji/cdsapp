4c764337bf31c72aba66e1eebf2ce3aedbc7786a
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-19 13:34:43 +0900

    add:Postモデル、投稿の関連付けのテスト記述

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
 
   let(:user) { create(:user) }
-  let(:posts) { create_list(:post, 3, user: user) }
+  let(:post) { create(:post, user: user) }
   let(:user_favorite) { create(:user) }
   let(:post_favorite) { create(:post, user: user_favorite) }
   let(:favorite) { create(:favorite, user_id: user_favorite.id, post_id: post_favorite.id) }
@@ -11,9 +11,11 @@ RSpec.describe Post, type: :model do
   describe '投稿の関連付けに関するテスト' do  
 
     it 'Userモデルとの紐付けが正しくできていること' do
+      expect([post.user]).to include user
     end
 
     it 'Favoriteモデルとの紐付けが正しくできていること' do
+      expect(favorite).to be_valid
     end
   end
 
