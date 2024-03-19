8dba63ea6116dea0db1f059998181a7f60c9e400
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-19 11:21:46 +0900

    add:postモデルのテストを書き起こし、土台の作成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+  let(:posts) { create_list(:post, 3, user: user) }
+  let(:user_favorite) { create(:user) }
+  let(:post_favorite) { create(:post, user: user_favorite) }
+  let(:favorite) { create(:favorite, user_id: user_favorite.id, post_id: post_favorite.id) }
+
+  describe '投稿の関連付けに関するテスト' do  
+
+    it 'Userモデルとの紐付けが正しくできていること' do
+    end
+
+    it 'Favoriteモデルとの紐付けが正しくできていること' do
+    end
+  end
+
+  describe '投稿のバリデーションに関するテスト' do  
+
+    it '空のデータがあれば投稿できない状態にできていること' do
+    end
+
+    it 'bodyの文字制限が140以下であること' do
+    end
+
+    it 'ユーザーidが含まれること' do
+    end
+  end
+
+  describe '作成したメソッドに関するテスト' do  
+
+    it 'favorited?メソッド' do
+    end
+
+    it 'favorite_countメソッド' do
+    end
+  end
 end
