8bd67ef7f872f86496e27c7de3f2dbdaaf40fabd
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-22 09:53:57 +0900

    add:フォームの入力値が正常系のテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -9,19 +9,37 @@ RSpec.describe "Users", type: :system do
     describe 'ユーザー新規登録' do
       context 'フォームの入力値が正常' do
         it 'ユーザーの新規作成が成功する' do
-        visit new_user_registration_path
-        fill_in 'user[email]', with: 'email@example.com'
-        fill_in 'user[password]', with: 'password'
-        fill_in 'user[password_confirmation]', with: 'password'
-        click_button 'Sign up'
-        expect(page).to have_content 'アカウント登録が完了しました'
+          visit new_user_registration_path
+          fill_in 'user[name]', with: 'example_man'
+          fill_in 'user[email]', with: 'email@example.com'
+          fill_in 'user[password]', with: 'password'
+          fill_in 'user[password_confirmation]', with: 'password'
+          click_button 'Sign up'
+          expect(page).to have_content 'アカウント登録が完了しました'
         end
       end
       context 'メールアドレスが未入力' do
-        it 'ユーザーの新規作成が失敗する'
+        it 'ユーザーの新規作成が失敗する' do
+          visit new_user_registration_path
+          fill_in 'user[name]', with: 'example_man'
+          fill_in 'user[email]', with: nil
+          fill_in 'user[password]', with: 'password'
+          fill_in 'user[password_confirmation]', with: 'password'
+          click_button 'Sign up'
+          expect(page).to have_content 'メールアドレスが入力されていません'
+        end
       end
       context '登録済のメールアドレスを使用' do
-        it 'ユーザーの新規作成が失敗する'
+        it 'ユーザーの新規作成が失敗する' do
+          visit new_user_registration_path
+          existed_user = create(:user)
+          fill_in 'user[name]', with: 'example_man'
+          fill_in 'user[email]', with: existed_user.email
+          fill_in 'user[password]', with: 'password'
+          fill_in 'user[password_confirmation]', with: 'password'
+          click_button 'Sign up'
+          expect(page).to have_field 'user[email]', with: existed_user.email
+        end
       end
     end
   end
