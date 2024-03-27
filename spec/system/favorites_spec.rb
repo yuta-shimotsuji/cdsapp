0ac36214d86e45150ccc18cab816bc68a9900ae7
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-27 15:40:29 +0900

    add:いいね機能の統合テスト追加

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Favorites", type: :system do
     visit new_user_session_path
     @user = create(:user, email: 'email@example.com', password: 'password', password_confirmation: 'password')
     @post = create(:post, user: @user)
-    @Favorite = create(:favorite, user_id: @user.id, post_id: @post.id)
+    @favorite = create(:favorite, user_id: @user.id, post_id: @post.id)
     fill_in 'user[email]', with: 'email@example.com'
     fill_in 'user[password]', with: 'password'
     click_button 'Log in'
@@ -15,10 +15,17 @@ RSpec.describe "Favorites", type: :system do
 
   describe 'いいね機能について' do
     context 'いいねしていない状態でボタンをクリック' do
-      it 'いいねが追加される' 
+      it 'いいねが追加される' do
+        click_link '❤︎' #いいねを消す
+        click_link '❤︎'
+        expect(@post.favorite_count(@post)).to eq 1
+      end
     end
     context 'いいね済みの状態でボタンをクリック' do
-      it 'いいねが削除される' 
+      it 'いいねが削除される' do
+        click_link '❤︎'
+        expect(@post.favorite_count(@post)).to eq 0
+      end
     end
   end
 end
