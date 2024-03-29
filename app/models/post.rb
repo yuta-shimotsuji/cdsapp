80e8fa7b719c455a54d5d986c190efb9887cf69f
yuta-shimotsuji <moshimo0310@gmail.com>
2024-03-29 21:45:29 +0900

    add:postモデルのバリデーション更新
    price,count,play

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :body, :address, presence: true
+  validates :title, :body, :address, :count, :play, :price, {presence: true}
   validates :body, length: { maximum: 140 }
   validates :user_id, {presence: true}
   belongs_to :user
