9d293166c75f557333ada248655ebfca62884072
yuta-shimotsuji <moshimo0310@gmail.com>
2024-04-03 22:36:36 +0900

    fix:ゲストログインの手法変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,13 +8,4 @@ class HomesController < ApplicationController
 
   def how_to_use_the_app
   end
-
-  def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲスト"
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
