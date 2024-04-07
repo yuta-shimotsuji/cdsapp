a9620848aadc126e2ccd96b5c41cb61f12bad9e1
yuta-shimotsuji <moshimo0310@gmail.com>
2024-04-07 15:17:38 +0900

    fix:Userモデルのnameカラムにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
+  validates :name, {presence: true}
 
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
