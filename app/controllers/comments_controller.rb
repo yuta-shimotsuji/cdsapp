ce3a286c95203bf08a65476197bfc51f07edf9f9
yuta-shimotsuji <moshimo0310@gmail.com>
2024-04-14 16:46:47 +0900

    fix:未ログインで投稿を行うと起こるエラーの解消

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,27 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.post_id = @post.id
-    if @comment.save
-      redirect_to post_path(@post)
+    if current_user != nil
+      @comment.user_id = current_user.id
+      @comment.post_id = @post.id
+      if @comment.save
+        redirect_to post_path(@post)
+      else
+        flash[:alert] = 'コメントに失敗しました'
+        redirect_to post_path(@post)
+      end
     else
-      flash[:alert] = 'コメントに失敗しました'
-      redirect_to post_path(@post)
+      @user = User.find(@post.user_id)
+      @latLng_test = Geocoder.search(@post.address)
+      @comment = Comment.new
+      @post_comments = @post.comments
+      if @latLng_test.present?
+        @latLng = Geocoder.search(@post.address).first.coordinates
+      else
+        redirect_to root_path, notice: '住所が不正のため表示できませんでした'
+      end
+      flash[:alert] = '投稿にはログインが必要です'
+      render "posts/show"
     end
   end
 
