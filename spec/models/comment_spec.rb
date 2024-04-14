c8bf1c5c9e703350690ccf23d1b09fb5bc33339f
yuta-shimotsuji <moshimo0310@gmail.com>
2024-04-14 18:11:27 +0900

    fix:Commentモデルのモデルスペックエラーを解消

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe Comment, type: :model do
   end
   
   describe 'コメント投稿に関するテスト' do
-    context 'コメントが保存できる場合'
+    context 'コメントが保存できる場合' do
       it 'コメントが入力済みであれば投稿できる' do
         expect(@comment).to be_valid
       end
     end
 
-    context 'コメントが保存できない場合'
+    context 'コメントが保存できない場合' do
       it 'コメントが空では投稿できない' do
         @comment.content = nil
         expect(@comment).not_to be_valid
