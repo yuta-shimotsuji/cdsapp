6e48cfba0de124fc77cb21ee36d008374f4250d3
yuta-shimotsuji <moshimo0310@gmail.com>
2024-04-29 16:18:39 +0900

    delete機能のエラー

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,9 +20,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
+    @post = Post.find(params[:id])
     @post_comments = @post.comments
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    Comment.find_by(id: params[:id], post_id: @post.id).destroy
     flash[:notice] = 'コメントを削除しました'
     redirect_to post_path(@post)
   end
